15218aa2102b0f72d335b262502a22fe2bfb894c
vehktaur <victorakhihiero@gmail.com>
2024-06-21 11:58:43 +0100

    made changes

--- src/components/About/ProvidersInAbout.jsx
+++ src/components/About/ProvidersInAbout.jsx
@@ -55,7 +55,7 @@ const ProvidersInAbout = () => {
     <div className="px-5">
       <div className="max-w-7xl mx-auto text-center">
         <h2 className="heading mb-4">Meet Our Providers</h2>
-        <p className="~mt-4/6 ~mb-6/[3.25rem] max-w-[65.75rem] mx-auto">
+        <p className="~mt-4/6 ~mb-6/[3.25rem] max-w-[65.75rem] mx-auto text-center">
           We believe in providing compassionate and comprehensive mental health
           care to our clients, and we recognize that each person is unique and
           requires personalized care. We understand that seeking therapy can be
